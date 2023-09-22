6ecb83cb45fb31cbf823d79c3cc8194eb63324de
hannguu <95076379+hannguu@users.noreply.github.com>
2023-09-22 23:57:27 +0700

    push3

---
+++ admin.controller/updateServlet.java
@@ -0,0 +1,137 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package admin.controller;
+
+import dal.DishDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+import model.Dish;
+
+/**
+ *
+ * @author Asus
+ */
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024, // 1 MB
+        maxFileSize = 1024 * 1024 * 10, // 10 MB
+        maxRequestSize = 1024 * 1024 * 50 // 50 MB
+)
+public class updateServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet updateServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet updateServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            int id = Integer.parseInt(request.getParameter("dish_id"));
+
+            // get a person from DB
+            DishDAO dishDAO = new DishDAO();
+            Dish dish = dishDAO.getDish(id);
+            request.setAttribute("dish", dish);
+
+            // get list address from DB
+            request.getRequestDispatcher("admin.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(updateServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            int id = Integer.parseInt(request.getParameter("dish_idd"));
+            DishDAO dishDAO = new DishDAO();
+            Dish dish = dishDAO.getDish(id);
+            request.setAttribute("dish", dish);
+            String name = (String) request.getParameter("namee");
+            String infor = (String) request.getParameter("inforr");
+            int price = Integer.parseInt(request.getParameter("pricee"));
+            Part part = request.getPart("imagee");
+            String realPath = request.getServletContext().getRealPath("/img");
+            String image = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+            part.write(realPath + "/" + image);
+            String comment = (String) request.getParameter("commentt");
+            String typeOfDish = (String) request.getParameter("typeOfDishh");
+            dishDAO.update(id, name, infor, price, image, comment, typeOfDish);
+
+            response.sendRedirect("ListProductServlet");
+//        request.getRequestDispatcher("list.jsp").forward(request, response);
+        } catch (Exception ex) {
+            String errorMessage = "Lỗi xảy ra khi thực hiện hành động: " + ex.getMessage();
+            request.setAttribute("errorMessage", errorMessage);
+            response.sendRedirect("error.jsp");
+            Logger.getLogger(updateServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
